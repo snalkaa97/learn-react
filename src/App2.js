3818abfbf4a878d4cb20af80002e13f864898ab1
Alka <sfnalkatiri@gmail.com>
2020-12-24 19:02:43 +0700

    App2.js -> with function useState

--- src/App2.js
+++ src/App2.js
@@ -1,51 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 // import Person from "./Person/Person";
 import UserInput from "./UserInput/UserInput";
 import UserOutput from "./UserOutput/UserOutput";
 
-class App extends Component {
-	state = {
+const App = () => {
+	const [usernameState, setUsernameState] = useState({
 		username: "snalkaa",
-	};
+	});
 
-	// setUsername = (newUsername) => {
-	// 	this.setState({
-	// 		username: newUsername,
-	// 	});
-	// };
-
-	nameChangedHandler = (event) => {
-		this.setState({
+	const nameChangedHandler = (event) => {
+		setUsernameState({
 			username: event.target.value,
 		});
 	};
 
-	render() {
-		const style = {
-			backgroundColor: "black",
-			font: "inherit",
-			border: "1px solid #ffff",
-			color: "white",
-		};
-
-		return (
-			<div className="App" style={style}>
-				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
-				<UserInput
-					changed={this.nameChangedHandler}
-					currentName={this.state.username}
-				/>
-				<UserOutput username={this.state.username} />
-				<UserOutput username={this.state.username} />
-				<UserOutput username="alka123" />
-			</div>
-		);
-		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-	}
-}
-
+	return (
+		<div className="App">
+			<h1>Hi, I'm a React App</h1>
+			<p>This is really working!</p>
+			<UserInput
+				changed={nameChangedHandler}
+				currentName={usernameState.username}
+			/>
+			<UserOutput username={usernameState.username} />
+			<UserOutput username={usernameState.username} />
+			<UserOutput username="alka123" />
+		</div>
+	);
+};
 // class App extends Component {
 // 	state = {
 // 		persons: [
