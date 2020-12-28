6b807cf01cab8a33f7dc07ec4715b255e3328784
Alka <sfnalkatiri@gmail.com>
2020-12-28 21:11:25 +0700

    assignment list and conditional

---
+++ src/CharComponent/Char.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./char.css";
+
+const CharComponent = (props) => {
+	const style = {
+		display: "inline-block",
+		padding: "16px",
+		margin: "16px",
+		border: "1px solid black",
+		textAlign: "center",
+	};
+
+	return (
+		<div style={style} onClick={props.clicked}>
+			{props.character}
+		</div>
+	);
+};
+export default CharComponent;
